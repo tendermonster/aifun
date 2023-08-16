00ea09736513428ee677c8c8051a7f40213319c8
Artiom Blinovas <25306286+tendermonster@users.noreply.github.com>
2023-08-16 10:59:34 +0200

    some updates to traning processs

--- tinker.py
+++ tinker.py
@@ -1,4 +1,5 @@
 import torch
+import random
 import torch.nn as nn
 
 
@@ -13,25 +14,7 @@ def make_layer(in_planes, planes, num_blocks, stride):
 
 
 if __name__ == "__main__":
-    # Create some example data
-    input = torch.randn(3, 5, requires_grad=True)
-    print(input)
-    target = torch.empty(3, dtype=torch.long).random_(5)
-    print(target)
-    num_classes = 10
-    batch_size = 1
-
-    # Simulated predicted logits and target labels
-    logits = torch.randn(batch_size, num_classes)
-    print(logits)
-    target_labels = torch.randint(0, num_classes, (batch_size,))
-    print(target_labels)
-    print(target_labels.dtype)
-
-    # Instantiate the CrossEntropyLoss
-    criterion = nn.CrossEntropyLoss()
-
-    # Calculate the loss
-    loss = criterion(logits, target_labels)
-
-    print("Loss:", loss.item())
+    a = [(1, 2), (3, 4), (5, 6)]
+    print(a)
+    random.shuffle(a)
+    print(a)
--- train.py
+++ train.py
@@ -34,31 +34,45 @@ if __name__ == "__main__":
     criterion = CrossEntropyLoss()
     optimizer = SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+    net.train()  # setting this changes behaviour of some modules like e.g dropout and batchnorm
     save = True
     # todo make batch size
     for epoch in range(30):  # loop over the dataset multiple times
         running_loss = 0.0
+        correct = 0
+        total = 0
         for i, data in enumerate(dataloader):
             # todo make the thing with
-            input, label = data
+            input, targets = data
             if input.dim() == 3:
                 input = input.unsqueeze(0)
-            if label.dim() == 0:
-                label = label.unsqueeze(0)
+            if targets.dim() == 0:
+                targets = targets.unsqueeze(0)
+            if torch.cuda.is_available():
+                input = input.to("cuda")
+                targets = targets.to("cuda")
             # zero the parameter gradients
             optimizer.zero_grad()
-
             # forward + backward + optimize
             outputs = net(input)
-            loss = criterion(outputs, label)
+            loss = criterion(outputs, targets)
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
-            if i % 20 == 19:  # print every 20 mini-batches
-                print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            if i >= int(2000 / batch_size):  # print every 20 mini-batches
+                print(f"minibatch {int(2000 / batch_size)}")
+                print(
+                    f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / int(2000 / batch_size):.8f}"
+                )
+                print(f"Accuracy: {100 * correct / total:.2f}%")
                 running_loss = 0.0
+                correct = 0
+                total = 0
 
     print("Finished Training")
 
